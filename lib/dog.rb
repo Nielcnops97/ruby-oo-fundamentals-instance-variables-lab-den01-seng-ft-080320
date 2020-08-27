1ccb8b35f09875437dfd062ea61f22c381512c44
Niel Cnops <nielcnops@outlook.com>
2020-08-27 21:22:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,6 @@ class Dog
         @dog_name = dog_name
       end
     
-      def name
-        @this_dogs_name
-    end   
-    lassie = Dog.new
-    lassie.name = "Lassie"
-    puts lassieame.n
-
-
     
 
 end
